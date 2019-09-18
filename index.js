c2b92159283c63b9b5f07f484b8e9cebf9ac9974
SHREYA CHAUHAN <47405473+shreya0404@users.noreply.github.com>
2019-09-18 02:02:07 -0500

    uploaded all

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let cors = require('cors');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+const https = require('https')
+
+require('dotenv').config();
+
+let PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use('/', express.static(__dirname + '/www'));
+app.get('/docuData', docuData);
+
+server.listen(PORT, () => console.log(`Express server listening on ${ PORT }`));
+
+let docuData = (req, res) =>{
+console.log(docuData);
+};
