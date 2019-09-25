8ceb58111559de3f2e74cd8963d7386c270d85fc
SHREYA CHAUHAN <47405473+shreya0404@users.noreply.github.com>
2019-09-25 01:11:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ let cors = require('cors');
 let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const https = require('https')
 
 require('dotenv').config();
 
 let PORT = process.env.PORT || 3000;
-app.use(bodyParser);
+//app.use(bodyParser);
 
 app.use(cors());
 app.use('/', express.static(__dirname + '/www'));
@@ -18,7 +18,6 @@ app.post('/docuData', docuData);
 server.listen(PORT, () => console.log(`Express server listening on ${ PORT }`));
 
 function docuData(req, res){
-console.dir(req.body);
-console.log(req.body);
+console.log(req.rawBody);
   res = "test";
 }
